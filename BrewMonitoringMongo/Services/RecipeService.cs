a21ee265d6b4d326bb8287c551bc0630e87d46ea
pikaille <vincentpcrd@gmail.com>
2015-06-04 22:54:48 -0400

    a couple file at a time commit

---
+++ BrewMonitoringMongo/Services/RecipeService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BrewMonitoring.Entities;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using MongoDB.Driver;
+
+namespace BrewMonitoring.Services
+{
+	public class RecipeService : EntityService<Recipe>
+	{
+		public const string COLLECTION_NAME = "Recipe";
+		public RecipeService ()
+		{
+		}
+
+		#region implemented abstract members of EntityService
+
+		public override string GetCollectionName ()
+		{
+			return COLLECTION_NAME;
+		}
+
+		#endregion
+	}
+}
+
